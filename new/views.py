b5a3afb93570827e5eaefbfa8cf963ce78a98d28
HamseYusuf <xamseyuusuf83@gmail.com>
2024-05-25 10:04:15 +0300

    models

--- new/views.py
+++ new/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 
 def home(request):
-    return render(request , 'index.html')
+    books = Book.objects.all()  # SELECT * FROM BOOK
+    context = {
+        'books' : books
+    }
+
+    return render(request , 'index.html' , context)
 
 def about(request):
     return render(request , 'about.html')
